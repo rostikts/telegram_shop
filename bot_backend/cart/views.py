72166dd4a70aaba56f2999dd2da8468d6c96bb74
rostik <rostiktsyapura@gmail.com>
2021-09-21 22:59:22 +0300

    DRY refactor

--- bot_backend/cart/views.py
+++ bot_backend/cart/views.py
@@ -1,69 +1,51 @@
+from django.http import HttpRequest
+
 from rest_framework.views import APIView
 from rest_framework import status
 from rest_framework.response import Response
 
-
-from .models import Cart, CartItem
 from .serializers import CartSerializer, CartItemSerializer
+from .services import CartService
 
 
 class CartView(APIView):
-    def post(self, request) -> Response:
-        if Cart.objects.filter(customer=request.data.get('customer')).filter(checked_out=False).first():
-            return Response('You already have an active cart', status=status.HTTP_400_BAD_REQUEST)
-        serializer = CartSerializer(data=request.data)
-        if serializer.is_valid():
-            serializer.create(request.data)
-            return Response(serializer.data, status=status.HTTP_201_CREATED)
-        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+    def post(self, request: HttpRequest) -> Response:
+        data = CartService.init_cart(request.data)
+        return Response(data, status=status.HTTP_201_CREATED)
 
 
 class CartDetailView(APIView):
-    def get(self, request, cart_id):
-        cart = Cart.objects.filter(id=cart_id).filter(checked_out=False).first()
-        serializer = CartSerializer(cart)
-        return Response(serializer.data)
+    def get(self, request: HttpRequest, cart_id):
+        cart = CartService.get_active_cart_by_id(cart_id)
+        data = CartSerializer(instance=cart).data
+        return Response(data=data, status=status.HTTP_200_OK)
 
 
 class CartItemsView(APIView):
-    def get(self, request, cart_id):
-        items = CartItem.objects.filter(cart__id=cart_id).all()
-        serializer = CartItemSerializer(instance=items, many=True)
-        return Response(serializer.data, status.HTTP_200_OK)
+    def get(self, request: HttpRequest, cart_id):
+        items = CartService().get_all_items_by_cart_id(cart_id)
+        data = CartItemSerializer(instance=items, many=True).data
+        return Response(data, status.HTTP_200_OK)
 
-    def post(self, request, cart_id: int):
+    def post(self, request: HttpRequest, cart_id: int):
         data = request.data
-        data['cart'] = cart_id
-        serializer = CartItemSerializer(data=data)
-        if serializer.is_valid():
-            print(serializer.validated_data)
-            serializer.create(serializer.validated_data)
-            return Response(serializer.data, status=status.HTTP_201_CREATED)
-        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+        response = CartService().add_item_to_cart(cart_id, data)
+        return Response(response, status=status.HTTP_201_CREATED)
 
 
 class CartItemDetails(APIView):
-    def get(self, request, cart_id: int, item_id: int):
-        item = CartItem.objects.filter(cart__id=cart_id).filter(id=item_id).first()
-        print(item)
-        serializer = CartItemSerializer(item)
-        print(serializer)
-        return Response(serializer.data, status=status.HTTP_200_OK)
+    def get(self, request: HttpRequest, cart_id: int, item_id: int):
+        item = CartService().get_item_from_cart_by_id(cart_id, item_id)
+        data = CartItemSerializer(item).data
+        return Response(data, status=status.HTTP_200_OK)
 
-    def put(self, request, cart_id: int, item_id: int):
-        item = CartItem.objects.filter(id=item_id).first()
+    def put(self, request: HttpRequest, cart_id: int, item_id: int):
         data = request.data
-        data['cart'] = cart_id
-        serializer = CartItemSerializer(instance=item, data=data, partial=True)
-        if serializer.is_valid():
-            serializer.update(item, serializer.validated_data)
-            return Response(serializer.data, status=status.HTTP_201_CREATED)
-        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+        response = CartService.edit_item_in_cart(data, cart_id, item_id).data
+        return Response(response, status=status.HTTP_201_CREATED)
 
 
 class CompleteActiveChatView(APIView):
-    def put(self, request, chat_id):
-        cart = Cart.objects.filter(customer__chat_id=chat_id).filter(checked_out=False).first()
-        cart.checked_out = True
-        cart.save()
+    def put(self, request: HttpRequest, cart_id):
+        CartService.complete_order(cart_id)
         return Response(status=status.HTTP_204_NO_CONTENT)
